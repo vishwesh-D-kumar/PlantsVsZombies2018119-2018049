5426a531984845e00278299ef36481f3bff7fe94
vishweshdkumar <vishwesh18119@iiitd.ac.in>
2019-11-01 21:16:18 +0530

    Added Background

--- Plants_vs_Zombies/src/loginscreen/Main.java
+++ Plants_vs_Zombies/src/loginscreen/Main.java
@@ -1,4 +1,23 @@
 package loginscreen;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("loginscreen.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 }
