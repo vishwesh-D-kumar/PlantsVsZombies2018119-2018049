90f5e3b8dfbff7d0f705fee70c5ab603ca7aae3a
vishweshdkumar <vishwesh18119@iiitd.ac.in>
2019-11-03 20:34:37 +0530

    Updated Classes

---
+++ Plants_vs_Zombies/src/entity/sun.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class sun extends Entity {
+    sun(int x_position, int y_position, int MAX_HEALTH, int speed, String image_path) {
+        super(x_position, y_position, MAX_HEALTH, speed, "/sprites/entity_images/Sun.png");
+    }
+}
